956b62828cf16316624f86777244ae11dc54db14
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-16 10:23:47 +0530

    initial

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: { // [!code ++] // [!code focus]
+    serverComponentsExternalPackages: ['grammy'], // [!code ++] // [!code focus]
+  }, // [!code ++] // [!code focus]
 };
 
 export default nextConfig;

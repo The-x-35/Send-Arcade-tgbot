d9ae982a75806eab29f09cd791f111cd40658125
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-16 23:01:49 +0530

    using rps

--- src/app/tools/rps.ts
+++ src/app/tools/rps.ts
@@ -7,43 +7,44 @@ export async function rps(
   choice: "R" | "P" | "S",
 ): Promise<string> {
   try {
-    // const res = await fetch(
-    //   `https://rps.sendarcade.fun/api/actions/backend?amount=${amount}&choice=${choice}&player=B`,
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application"
-    //     },
-    //     body: JSON.stringify({
-    //       account: agent.wallet.publicKey.toBase58(),
-    //     }),
-    //   },
-    // );
+    const res = await fetch(
+      `https://rps.sendarcade.fun/api/actions/backend?amount=${amount}&choice=${choice}&player=B`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application"
+        },
+        body: JSON.stringify({
+          account: agent.wallet.publicKey.toBase58(),
+        }),
+      },
+    );
 
-    // const data = await res.json();
+    const data = await res.json();
 
-    // const txn = VersionedTransaction.deserialize(
-    //   Buffer.from(data.transaction, "base64"),
-    // );
-    // const { blockhash } = await agent.connection.getLatestBlockhash();
-    // txn.message.recentBlockhash = blockhash;
+    const txn = VersionedTransaction.deserialize(
+      Buffer.from(data.transaction, "base64"),
+    );
+    const { blockhash } = await agent.connection.getLatestBlockhash();
+    txn.message.recentBlockhash = blockhash;
 
-    // // Sign and send transaction
-    // txn.sign([agent.wallet]);
-    // const signature = await agent.connection.sendTransaction(txn, {
-    //   preflightCommitment: "confirmed",
-    //   maxRetries: 3,
-    // });
+    // Sign and send transaction
+    txn.sign([agent.wallet]);
+    const signature = await agent.connection.sendTransaction(txn, {
+      preflightCommitment: "confirmed",
+      maxRetries: 3,
+    });
 
-    // const latestBlockhash = await agent.connection.getLatestBlockhash();
-    // await agent.connection.confirmTransaction({
-    //   signature,
-    //   blockhash: latestBlockhash.blockhash,
-    //   lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-    // });
-    return "done";
+    const latestBlockhash = await agent.connection.getLatestBlockhash();
+    await agent.connection.confirmTransaction({
+      signature,
+      blockhash: latestBlockhash.blockhash,
+      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+    });
+    return signature;
   } catch (error: any) {
     console.error(error);
-    throw new Error(`RPS game failed: ${error.message}`);
+    return error.message.toString();
+    // throw new Error(`RPS game failed: ${error.message}`);
   }
 }

dfbd99910ae7dbd7f951fdc1f7eee9faa2bbe47f
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-17 20:33:35 +0530

    test

--- src/app/tools/rps.ts
+++ src/app/tools/rps.ts
@@ -36,7 +36,7 @@ export async function rps(
 
             // Sign and send transaction
             txn.sign(KEYPAIR);
-            await sendAndConfirmTransaction(connection, txn, [KEYPAIR]);
+            sendAndConfirmTransaction(connection, txn, [KEYPAIR]);
             // return outcome(agent,href);
             return href;
         } else {

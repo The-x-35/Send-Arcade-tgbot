5ecb46ad8332b455a83f49510729cda54e971c3c
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-17 20:21:18 +0530

    testing outcome file

--- src/app/tools/rps.ts
+++ src/app/tools/rps.ts
@@ -29,21 +29,47 @@ export async function rps(
         const data = await res.json();
         console.log(data);
         const href = data.links?.next?.href;
-        return href || "failed";
-        // if (data.transaction) {
-        //     console.log(data.message);
-        //     const txn = Transaction.from(Buffer.from(data.transaction, "base64"));
+        
+        if (data.transaction) {
+            console.log(data.message);
+            const txn = Transaction.from(Buffer.from(data.transaction, "base64"));
 
-        //     // Sign and send transaction
-        //     txn.sign(KEYPAIR);
-        //     const signature = await sendAndConfirmTransaction(connection, txn, [KEYPAIR]);
-
-        //     return signature.toString();
-        // } else {
-        //     return "failed";
-        // }
+            // Sign and send transaction
+            txn.sign(KEYPAIR);
+            sendAndConfirmTransaction(connection, txn, [KEYPAIR]);
+            return outcome(agent,href);
+        } else {
+            return "failed";
+        }
     } catch (error: any) {
         console.error(error);
         throw new Error(`RPS game failed: ${error.message}`);
     }
+}
+async function outcome(agent: SolanaAgentKit,href: string):Promise<string> {
+    try {
+        const connection = new Connection(`https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`);
+        const KEYPAIR = agent.wallet;
+        const ADDRESS = KEYPAIR.publicKey;
+        const PRIVATE_KEY = KEYPAIR.secretKey;
+        const res = await fetch(
+            `https://rps.sendarcade.fun${href}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    account: ADDRESS.toBase58(),
+                }),
+            },
+        );
+
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        console.error(error);
+        throw new Error(`RPS outcome failed: ${error.message}`);
+    }
+
 }

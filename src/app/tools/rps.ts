97e52e90ed57bfbe409db118168f8518dea4fad9
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-17 18:19:15 +0530

    testing bot endpoint with heliuys

--- src/app/tools/rps.ts
+++ src/app/tools/rps.ts
@@ -1,41 +1,45 @@
-import { sendAndConfirmTransaction, Transaction, VersionedTransaction } from "@solana/web3.js";
+import { Connection, sendAndConfirmTransaction, Transaction, VersionedTransaction } from "@solana/web3.js";
 import { SolanaAgentKit } from "solana-agent-kit";
 
 export async function rps(
-  agent: SolanaAgentKit,
-  amount: number,
-  choice: "rock" | "paper" | "scissors",
+    agent: SolanaAgentKit,
+    amount: number,
+    choice: "rock" | "paper" | "scissors",
 ): Promise<string> {
-  try {
-    const res = await fetch(
-      `https://rps.sendarcade.fun/api/actions/backend?amount=${amount}&choice=${choice}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          account: agent.wallet.publicKey.toBase58(),
-        }),
-      },
-    );
+    try {
+        const connection = new Connection(`https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`);
+        const KEYPAIR = agent.wallet;
+        const ADDRESS = KEYPAIR.publicKey;
+        const PRIVATE_KEY = KEYPAIR.secretKey;
+        const res = await fetch(
+            `https://rps.sendarcade.fun/api/actions/bot?amount=${amount}&choice=${choice}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    account: ADDRESS.toBase58(),
+                }),
+            },
+        );
 
-    const data = await res.json();
-    console.log(data);
-    if (data.transaction) {
-        console.log(data.message);
-    const txn = Transaction.from(Buffer.from(data.transaction, "base64"));
+        const data = await res.json();
+        console.log(data);
+        if (data.transaction) {
+            console.log(data.message);
+            const txn = Transaction.from(Buffer.from(data.transaction, "base64"));
 
-    // Sign and send transaction
-    txn.sign(agent.wallet);
-    const signature = await sendAndConfirmTransaction(agent.connection, txn, [agent.wallet]);
+            // Sign and send transaction
+            txn.sign(KEYPAIR);
+            const signature = await sendAndConfirmTransaction(connection, txn, [KEYPAIR]);
 
-    return signature;
-    } else {
-        return "failed";
+            return signature;
+        } else {
+            return "failed";
+        }
+    } catch (error: any) {
+        console.error(error);
+        throw new Error(`RPS game failed: ${error.message}`);
     }
-  } catch (error: any) {
-    console.error(error);
-    throw new Error(`RPS game failed: ${error.message}`);
-  }
 }

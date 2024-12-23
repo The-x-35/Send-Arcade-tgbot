76771c653ce7a58c9e125926d7c5fc23f8f43df0
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-23 21:29:45 +0530

    test

--- src/app/tools/rps.ts
+++ src/app/tools/rps.ts
@@ -153,7 +153,8 @@ async function outcome(agent: SolanaAgentKit, sig: string, href: string): Promis
             return title;
         }
         let next_href = data.links?.actions?.[0]?.href;
-        return title + "\n" + await won(agent, next_href)
+        won(agent, next_href)
+        return title
     } catch (error: any) {
         console.error(error);
         throw new Error(`RPS outcome failed: ${error.message}`);
@@ -178,12 +179,12 @@ async function won(agent: SolanaAgentKit, href: string): Promise<string> {
         if (data.transaction) {
             const txn = Transaction.from(Buffer.from(data.transaction, "base64"));
             txn.partialSign(agent.wallet);
-            await agent.connection.sendRawTransaction(txn.serialize(),{ preflightCommitment: 'confirmed', skipPreflight: true });        }
+            agent.connection.sendRawTransaction(txn.serialize(),{ preflightCommitment: 'confirmed', skipPreflight: true });        }
         else {
             return "Failed to claim prize.";
         }
         let next_href = data.links?.next?.href;
-        return await postWin(agent, next_href);
+        return postWin(agent, next_href);
     } catch (error: any) {
         console.error(error);
         throw new Error(`RPS outcome failed: ${error.message}`);
